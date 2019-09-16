7fcccbc75db46116dc830ebf800c42212d0e48b7
mateuszpijanowski <mateuszpijanowski0@gmail.com>
2019-09-16 21:49:25 +0200

    HotFix - Fixed ads

---
+++ adsAPI.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {AdMobBanner} from 'expo-ads-admob';
+
+const bannerAPI = 'ca-app-pub-1606849839773048/6304086761';
+
+export const BannerAd = () => (
+    <AdMobBanner
+        bannerSize="fullBanner"
+        adUnitID={bannerAPI}
+        testDeviceID="EMULATOR" />
+);
